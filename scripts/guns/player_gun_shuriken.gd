4a2a79e859e0aff97006127e95170ed6679d925e
Luiz Felippe de Amorim Bezerra <felippeluiz97@gmail.com>
2025-01-25 19:36:04 -0300

    Sei la

--- scripts/guns/player_gun_shuriken.gd
+++ scripts/guns/player_gun_shuriken.gd
@@ -2,6 +2,23 @@ extends "res://scripts/guns/player_gun.gd"
 
 const LAUNCH_FORCE := 250.0
 
-func _setup_bullet(new_bullet : Node2D) -> void:
+func _process(delta: float) -> void:
+	_timer = _timer - delta
+	if _timer > 0.0 or _stats.is_stunned or _stats.is_in_bubble:
+		return
+	
+	if Input.is_action_just_pressed(action):
+		var new_bullet := bullet.instantiate()
+		new_bullet.global_position = global_position + (offset if _stats.facing_right else -offset)
+		_launch_shuriken(new_bullet, true)
+		get_tree().current_scene.add_child(new_bullet)
+		_timer = cooldown
+
+func _launch_shuriken(new_bullet : Node2D, horizontal : bool) -> void:
 	var shur := new_bullet as Shuriken
-	shur._launch(LAUNCH_FORCE if _stats.facing_right else -LAUNCH_FORCE)
+	var force : Vector2 
+	if(horizontal):
+		force = Vector2(LAUNCH_FORCE if _stats.facing_right else -LAUNCH_FORCE,0)
+	else :
+		force = Vector2(0, LAUNCH_FORCE)
+	shur._launch(force)
