02209c91a3db9ed7fe9d4d3590b17d6839d2b12c
Luiz Felippe de Amorim Bezerra <felippeluiz97@gmail.com>
2025-01-25 23:12:22 -0300

    Shuriken chupeta

--- scripts/guns/shuriken.gd
+++ scripts/guns/shuriken.gd
@@ -1,7 +1,7 @@
 extends Area2D
 class_name Shuriken
 
-var _timer : float = 7.0
+var _timer : float = 0.8
 var _velocity : Vector2
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -27,6 +27,6 @@ func _physics_process(delta: float) -> void:
 		queue_free()
 		return
 	
-	_velocity.x = move_toward(_velocity.x, 0.0, delta * 100.0)
-	_velocity.y = move_toward(_velocity.y,0.0, delta*100.0)
+	#_velocity.x = move_toward(_velocity.x, 0.0, delta * 100.0)
+	#_velocity.y = move_toward(_velocity.y,0.0, delta*100.0)
 	global_position += _velocity * delta
